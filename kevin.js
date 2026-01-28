96a0d0f278ca245f5da530668fbc40ba07f3c627
NTElissa <ntihindukaelissa77@gmail.com>
2026-01-28 09:16:55 +0200

    Kevin The Cyber Codeer

---
+++ kevin.js
@@ -0,0 +1,14 @@
+let arr = [
+   FirstObject = {
+        name: "Kevin",
+        age: 20,
+        city: "KIGALI RWANDA"
+    },
+   SecondObject = {
+        name:"The cyber Coder",
+        hobby: "Coding",
+        career: "Software Developer"
+    }
+
+]
+console.log(arr[0]);
